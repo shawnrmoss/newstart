c8b1eac3bed48afbea7e96c3a9ab6bb58d13bd39
shawnrmoss <shawnrmoss@gmail.com>
2016-01-12 08:09:32 -0700

    linechart

--- src/app/common/linechart/linechart.ts
+++ src/app/common/linechart/linechart.ts
@@ -8,6 +8,15 @@ import 'angular';
     directives: []
 })
 export class LineChart {
+      
+  labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  series = ['Series A', 'Series B'];
+
+  data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  
   constructor(      
   ) {}  
       
